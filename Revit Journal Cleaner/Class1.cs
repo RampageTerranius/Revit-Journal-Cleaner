3e5e3466017c9fcdad9ed9dcfc9751449932ffd5
Tyler Brown <rampage_terranius@hotmail.com>
2019-05-10 11:33:24 +1000

    Add project files.

---
+++ Revit Journal Cleaner/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_Journal_Cleaner
+{
+    public class Class1
+    {
+    }
+}
